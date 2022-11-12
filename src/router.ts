acb4076a96ab07e40b684483d9aead25452ca74d
Davi <davi.frrs@outlook.com>
2022-11-12 14:00:35 -0300

    feat: routes and middlewares

---
+++ src/router.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Products
+ */
+router.get('/product', (request, response) => {
+    response.json({ message: "Hello" });
+});
+router.get('/product/:id', () => {});
+router.put('/product/:id', () => {});
+router.post('/product', () => {});
+router.delete('/product/:id', () => {});
+
+
+/**
+ * Updates
+ */
+router.get('/update', () => {});
+router.get('/update/:id', () => {});
+router.put('/update/:id', () => {});
+router.post('/update', () => {});
+router.delete('/update/:id', () => {});
+
+/**
+ * Update point
+ */
+router.get('/updatepoint', () => {});
+router.get('/updatepoint/:id', () => {});
+router.put('/updatepoint/:id', () => {});
+router.post('/updatepoint', () => {});
+router.delete('/updatepoint/:id', () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express from "express";
+import router from "./router";
+import morgan from "morgan";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.status(401);
+    res.send("Nope")
+});
 
 app.get("/", (request, response) => {
     console.log('hellow from express');
     response.status(200);
     response.json({ message: "Hello" });
 });
+
+app.use('/api', router);
 
 export default app;
